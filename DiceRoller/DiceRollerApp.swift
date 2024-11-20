0e78cbb1af6a83e0d3779b07926749190dc618d2
Pablo Porzio <pabloporzio97@gmail.com>
2024-11-20 17:34:03 -0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Pablo Porzio (Modak) on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
